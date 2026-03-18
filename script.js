23288ee0e5c80da8babe8c054fceceb0be75c943
JUAN MANUEL SABATHIE LOPEZ <49378703@ort.edu.ar>
2026-03-18 09:37:49 -0300

    commit 2

--- script.js
+++ script.js
@@ -37,8 +37,65 @@ console.log(validarPassword('Juanmitapro2009'));
 
 //Parte 2 — Arrays
 //Ejercicio 5 — Suma de array
-function sumarArray(numeros) {
-    return 
-}
+const sumarArray = unArray => 
+    {
+        let total = 0;
+        unArray.forEach(numero => {
+            total += numero;
+        });
+        return total;
+    }
 
 console.log(sumarArray([1, 2, 3, 4, 5]));
+
+//Ejercicio 6 — Número mayor de un array
+const numeroMayor = numeros => {
+    let numerofinal = numeros[0];
+    for (let i = 1; i < numeros.length; i++) {
+        if (numeros[i] > numerofinal) {
+            numerofinal = numeros[i];
+        }
+    }
+    return numerofinal;
+}
+console.log(numeroMayor([2,4,1,3,5,6,3,5,3,2,5,8,4,3]));
+
+
+//Ejercicio 7 — Filtrar pares
+
+const obtenerPares = numeros => {
+    return numeros.filter(numero => numero % 2 === 0);
+}
+console.log(obtenerPares([1,2,3,4,5,6]));
+
+
+
+//Parte 3 — Objetos
+//Ejercicio 8 — Descripción de usuario
+
+
+const usuario = {
+
+ nombre: "Ana",
+
+ edad: 20,
+
+ activo: false
+
+}
+describeUsuario(usuario);
+function describeUsuario(usuario) {
+    return `${usuario.nombre} tiene ${usuario.edad} años`;
+}
+console.log(describeUsuario(usuario));
+
+
+//Ejercicio 9 — Activar usuario
+function activarUsuario(usuario) {
+    usuario.activo = true;
+    return usuario;
+}
+console.log(activarUsuario(usuario));
+
+
+
